db44496a70b4be137c049714dc2a7eaabe92d644
pruthviraj9696 <kohpruthviraj9696@gmail.com>
2023-02-17 20:21:43 +0530

    assiment

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+let eva = document.getElementById("eva");
+let letterCount = getElementById("letterCount");
+
+eva.addEventListener("input",lettertext);
+
+function lettertext(){
+    let word = eva.value;
+    let letter = word.length;
+    letterCount.innerText = letter;
+}
