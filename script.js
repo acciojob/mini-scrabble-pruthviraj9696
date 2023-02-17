0737bbce1ac53f53f499f77258dd35e6c0345301
pruthviraj9696 <120453319+pruthviraj9696@users.noreply.github.com>
2023-02-17 06:54:33 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-//your code here
-let eva = document.getElementById("eva");
-let letterCount = getElementById("letterCount");
 
-eva.addEventListener("input",lettertext);
-
-function lettertext(){
-    let word = eva.value;
-    let letter = word.length;
-    letterCount.innerText = letter;
-}
