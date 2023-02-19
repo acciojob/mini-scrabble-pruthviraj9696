824b2dc969fb639743cde99d635c98dae7a4b90b
pruthviraj9696 <kohpruthviraj9696@gmail.com>
2023-02-19 14:38:32 +0530

    scribble

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-//your code here
-let eva = document.getElementById("eva");
-let letterCount = getElementById("letterCount");
+// //your code here
+// let eva = document.getElementById("eva");
+// let letterCount = getElementById("letterCount");
 
-eva.addEventListener("input",lettertext);
+// eva.addEventListener("input",lettertext);
 
-function lettertext(){
-    let word = eva.value;
-    let letter = word.length;
-    letterCount.innerText = letter;
+// function lettertext(){
+//     let word = eva.value;
+//     let letter = word.length;
+//     letterCount.innerText = letter;
+// }
+
+
+function getData() {
+    const name = document.getElementById('eva').value;
+    document.getElementById('letterCount').innerHTML = name.length;
 }
